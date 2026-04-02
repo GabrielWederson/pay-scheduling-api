ee57c597eb6f4d8edd79475216a76cd2d80e7031
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-04-02 15:06:44 -0300

    created: SchedulingRequestDTO

---
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/dto/SchedulingRequestDTO.java
@@ -0,0 +1,65 @@
+package io.github.gabrielwederson.pay_scheduler_api.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class SchedulingRequestDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String originAccount;
+
+    private String destinationAccount;
+
+    private Double value;
+
+    private LocalDateTime schedulingDate;
+
+    public SchedulingRequestDTO() {
+    }
+
+    public String getOriginAccount() {
+        return originAccount;
+    }
+
+    public void setOriginAccount(String originAccount) {
+        this.originAccount = originAccount;
+    }
+
+    public String getDestinationAccount() {
+        return destinationAccount;
+    }
+
+    public void setDestinationAccount(String destinationAccount) {
+        this.destinationAccount = destinationAccount;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getSchedulingDate() {
+        return schedulingDate;
+    }
+
+    public void setSchedulingDate(LocalDateTime schedulingDate) {
+        this.schedulingDate = schedulingDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SchedulingRequestDTO that = (SchedulingRequestDTO) o;
+        return Objects.equals(originAccount, that.originAccount) && Objects.equals(destinationAccount, that.destinationAccount) && Objects.equals(value, that.value) && Objects.equals(schedulingDate, that.schedulingDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(originAccount, destinationAccount, value, schedulingDate);
+    }
+}
