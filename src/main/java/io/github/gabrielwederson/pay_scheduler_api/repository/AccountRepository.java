38626d87c54766a371d87c7626bc0839fc56a664
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-04-02 15:01:03 -0300

    created: all repositorys of entities

---
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/repository/AccountRepository.java
@@ -0,0 +1,7 @@
+package io.github.gabrielwederson.pay_scheduler_api.repository;
+
+import io.github.gabrielwederson.pay_scheduler_api.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
---
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/repository/SchedulingRepository.java
@@ -0,0 +1,7 @@
+package io.github.gabrielwederson.pay_scheduler_api.repository;
+
+import io.github.gabrielwederson.pay_scheduler_api.model.Scheduling;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchedulingRepository extends JpaRepository<Scheduling, Long> {
+}
