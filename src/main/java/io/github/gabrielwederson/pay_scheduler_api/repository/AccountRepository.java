7cb2e752d089f6ad8df5d9e0870886e48bdbeb49
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-04-03 13:02:07 -0300

    created: a method findaccountbynumber at AccountRepository

--- src/main/java/io/github/gabrielwederson/pay_scheduler_api/repository/AccountRepository.java
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/repository/AccountRepository.java
@@ -2,6 +2,11 @@ package io.github.gabrielwederson.pay_scheduler_api.repository;
 
 import io.github.gabrielwederson.pay_scheduler_api.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("SELECT a FROM Account a WHERE a.numberAccount =:numberAccount")
+    Account findAccountByNumber(@Param("numberAccount")String numberAccount);
 }
--- src/main/java/io/github/gabrielwederson/pay_scheduler_api/repository/SchedulingRepository.java
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/repository/SchedulingRepository.java
@@ -4,4 +4,5 @@ import io.github.gabrielwederson.pay_scheduler_api.model.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchedulingRepository extends JpaRepository<Scheduling, Long> {
+
 }
