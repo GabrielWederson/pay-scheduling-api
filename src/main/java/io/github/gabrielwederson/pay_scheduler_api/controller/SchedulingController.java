452de0fc711f66a28b47b456326b2ca9654f84c5
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-04-04 13:27:47 -0300

    created: SchedulingControllerDocs, and Updated: pom, application, SchedulingController

--- src/main/java/io/github/gabrielwederson/pay_scheduler_api/controller/SchedulingController.java
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/controller/SchedulingController.java
@@ -1,8 +1,10 @@
 package io.github.gabrielwederson.pay_scheduler_api.controller;
 
+import io.github.gabrielwederson.pay_scheduler_api.controller.docs.SchedulingControllerDocs;
 import io.github.gabrielwederson.pay_scheduler_api.dto.SchedulingRequestDTO;
 import io.github.gabrielwederson.pay_scheduler_api.dto.SchedulingResponseDTO;
 import io.github.gabrielwederson.pay_scheduler_api.service.SchedulingService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,22 +13,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/scheduling/v1")
-public class SchedulingController {
+@Tag(name = "Appointments", description = "endpoints to management appointments")
+public class SchedulingController implements SchedulingControllerDocs {
 
     @Autowired
     SchedulingService schedulingService;
 
     @PostMapping
+    @Override
     public SchedulingResponseDTO createScheduling(@RequestBody SchedulingRequestDTO schedulingRequestDTO){
         return schedulingService.create(schedulingRequestDTO);
     }
 
     @GetMapping
+    @Override
     public List<SchedulingResponseDTO> findAllAppointments(){
         return schedulingService.findAll();
     }
 
     @DeleteMapping(value = "/{id}")
+    @Override
     public ResponseEntity<?> deleteScheduling(@PathVariable("id") Long id){
         schedulingService.delete(id);
         return ResponseEntity.noContent().build();
---
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/controller/docs/SchedulingControllerDocs.java
@@ -0,0 +1,70 @@
+package io.github.gabrielwederson.pay_scheduler_api.controller.docs;
+
+import io.github.gabrielwederson.pay_scheduler_api.dto.SchedulingRequestDTO;
+import io.github.gabrielwederson.pay_scheduler_api.dto.SchedulingResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+public interface SchedulingControllerDocs {
+
+    @Operation(summary = "Find All Appointments",
+            description = "Finds All Appointments",
+            tags = {"Appointments"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                            array = @ArraySchema(schema = @Schema(implementation = SchedulingResponseDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    List<SchedulingResponseDTO> findAllAppointments();
+
+    @Operation(summary = "Adds a new Scheduling",
+            description = "Adds a new scheduling by passing in a JSON representation of the Scheduling.",
+            tags = {"Appointments"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = SchedulingResponseDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    SchedulingResponseDTO createScheduling(@RequestBody SchedulingRequestDTO requestDTO);
+
+    @Operation(summary = "Deletes a Scheduling",
+            description = "Deletes a Scheduling by specific id and specific status",
+            tags = {"Appointments"},
+            responses = {
+                    @ApiResponse(
+                            description = "No Content",
+                            responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<?> deleteScheduling(@PathVariable("id") Long id);
+}
