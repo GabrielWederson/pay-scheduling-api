7fd49560a2cac5f154770ba6eca9f764704406e6
Gabriel Wederson <gabrielwederson582@gmail.com>
2026-04-04 12:44:20 -0300

    created: all methods in SchedulingController

--- src/main/java/io/github/gabrielwederson/pay_scheduler_api/controller/SchedulingController.java
+++ src/main/java/io/github/gabrielwederson/pay_scheduler_api/controller/SchedulingController.java
@@ -4,10 +4,10 @@ import io.github.gabrielwederson.pay_scheduler_api.dto.SchedulingRequestDTO;
 import io.github.gabrielwederson.pay_scheduler_api.dto.SchedulingResponseDTO;
 import io.github.gabrielwederson.pay_scheduler_api.service.SchedulingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/scheduling/v1")
@@ -19,5 +19,16 @@ public class SchedulingController {
     @PostMapping
     public SchedulingResponseDTO createScheduling(@RequestBody SchedulingRequestDTO schedulingRequestDTO){
         return schedulingService.create(schedulingRequestDTO);
+    }
+
+    @GetMapping
+    public List<SchedulingResponseDTO> findAllAppointments(){
+        return schedulingService.findAll();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> deleteScheduling(@PathVariable("id") Long id){
+        schedulingService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
